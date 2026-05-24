1599c104666e051c779754647a96e515e1740053
Adrika Das <adrikadas0709@gmail.com>
2026-05-24 20:38:06 +0530

    Configure Vercel output directory and serverless backend

--- backend/server.js
+++ backend/server.js
@@ -81,5 +81,7 @@ app.get(/.*/, (req, res) => {
 });
 /*Start Server */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
- 
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+module.exports = app;
